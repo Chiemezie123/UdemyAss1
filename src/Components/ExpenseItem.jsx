34d5a430dd001f4858930c56b2fb8276f37773e7
chiemezie nduaguba <anthonynduaguba123@gmail.com>
2023-08-04 14:59:09 +0100

    first assignment

---
+++ src/Components/ExpenseItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ExpenseData from './ExpenseData';
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+  return (
+    <div className='expense-item'>
+      <ExpenseData date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
